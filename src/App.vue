e1b40450b8230aa3231cad2af6b53aca11abf77b
RosyrRais <syr050301@qq.com>
2025-02-26 13:02:26 +0800

    fix(pages): 修复表格的错误逻辑 改用computed计算属性释义 添加路线表格表头

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <n-message-provider>
-    <router-view />
-  </n-message-provider>
+  <n-config-provider>
+    <n-message-provider>
+      <router-view />
+    </n-message-provider>
+  </n-config-provider>
+
 </template>
 
 <script setup lang="ts">
-import { NMessageProvider } from 'naive-ui';
+import { NMessageProvider, NConfigProvider } from 'naive-ui';
 </script>
