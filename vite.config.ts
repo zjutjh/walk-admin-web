d3d29f0bf983367976c93d3ebbefa788f0af0104
maojianhui <maojianhui2006@gmail.com>
2025-09-26 14:06:46 +0800

    chore: 通过env存储测试环境url并补充git ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: "/web/",
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://test.phlin.top/api/v1', // 目标 API 服务器
-  //       changeOrigin: true, // 允许跨域
-  //       rewrite: (path) => path.replace(/^\/api/, ''), // 可选：重写路径
-  //     },
-  //   },
-  // },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    base: '/',
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: env.VITE_API_URL, // 目标 API 服务器
+          changeOrigin: true, // 允许跨域
+          // rewrite: (path) => path.replace(/^\/api/, ''), // 可选：重写路径
+        },
+      },
+    },
+  }
 })
